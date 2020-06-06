bf2dbfe7b92cc44546e44cd5fe7ea7505756ce1b
Gabriel Anselmo <66230209+gabrielarc9@users.noreply.github.com>
2020-06-05 22:24:23 -0300

    Add files via upload

---
+++ public/scripts/intro.js
@@ -0,0 +1,65 @@
+// comments
+// document.write("Hello")
+// variaveis, tipos de dados
+// var myvar = "he"
+// const myconst = "he"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso é uma string"
+// const s2 = 'Isso tbm é uma string'
+// const s3 = `Isso é uma string tbm`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("Executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["Blue", "Green", 1, {name:"my name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de códigos
+// registrar função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// Executar
+// sayMyName("Gabriel")
+// sayMyName("Rebeca")
+
+// condicionais
+
+// const notaFinal = 4
+
+// if( notaFinal < 5 ){
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop - repetições
+
+// for (i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+//}
